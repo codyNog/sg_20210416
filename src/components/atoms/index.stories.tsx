3393cc4478b9eb643d0d9b3039abd1e0b7dc0284
codyNog <yoneyoneyone99@gmail.com>
2021-03-13 00:28:25 +0900

    story追加

---
+++ src/components/atoms/index.stories.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Atom } from "~/components/atoms";
+
+storiesOf("components", module).add("atoms", () => <Atom />);
