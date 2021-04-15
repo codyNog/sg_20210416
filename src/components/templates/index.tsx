ce475fd7185da587a53de1429d3796f8d2bccabf
codyNog <yoneyoneyone99@gmail.com>
2021-04-16 03:13:01 +0900

    drawerにリンク追加

--- src/components/templates/index.tsx
+++ src/components/templates/index.tsx
@@ -1,18 +1,26 @@
 import {
   Drawer as DrawerWrapper,
   DrawerBody,
-  DrawerFooter,
-  DrawerHeader,
   DrawerOverlay,
   DrawerContent,
   DrawerCloseButton,
-  Button,
   BoxProps,
   Box,
-  IconButton
+  IconButton,
+  Flex
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { HamburgerIcon } from "@chakra-ui/icons";
+import Link from "next/link";
+
+const links = [
+  { href: "/users", body: "ユーザー一覧" },
+  { href: "/users/create", body: "ユーザー作成" },
+  { href: "/agencies", body: "不動産業者一覧" },
+  { href: "/agencies/create", body: "不動産業者作成" },
+  { href: "/properties", body: "不動産一覧" },
+  { href: "/properties/create", body: "不動産作成" }
+];
 
 const Header = (props: BoxProps) => <Box px={4} py={3} as={"nav"} {...props} />;
 const Wrapper = (props: BoxProps) => <Box {...props} />;
@@ -40,7 +48,21 @@ export const Template: React.FC = ({ children }) => {
         <DrawerOverlay>
           <DrawerContent>
             <DrawerCloseButton />
-            <DrawerBody></DrawerBody>
+            <DrawerBody>
+              <Flex
+                flexDirection={"column"}
+                alignItems={"center"}
+                justifyContent={"center"}
+              >
+                <Box>
+                  {links.map(({ href, body }) => (
+                    <Box key={href}>
+                      <Link href={href}>{body}</Link>
+                    </Box>
+                  ))}
+                </Box>
+              </Flex>
+            </DrawerBody>
           </DrawerContent>
         </DrawerOverlay>
       </DrawerWrapper>
